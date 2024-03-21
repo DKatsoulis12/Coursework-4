81173b3c405cb58e56b65737b0045f70458fe218
DKatsoulis12 <108552289+DKatsoulis12@users.noreply.github.com>
2024-03-21 23:06:23 +0000

    Add files via upload

---
+++ Coursework 4 FULLY WORKING, EFFICIENT BACKUP (clickUpdate in AppPane)/AppPane.java
@@ -0,0 +1,35 @@
+import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+
+/**
+ * Write a description of class AppPane here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class AppPane extends Pane
+{
+    // instance variables - replace the example below with your own
+    
+    
+    /**
+     * Constructor for objects of class AppPane
+     */
+    public AppPane()
+    {
+        // initialise instance variables
+       
+    }
+
+    public abstract void updatePane(ActionEvent event);
+    
+    public void clickUpdate(ActionEvent event) {
+        boolean f = AppGUI.getInstance().toggleButtonDisability();
+
+        if (f == false) {
+            updatePane(event);
+        } else {
+            AppGUI.getInstance().alertSetUp();
+        }
+    }
+}
