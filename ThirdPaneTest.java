d8c7f4e230a0121900c9172a7c8e87b59c984a58
Pluto-999 <97910129+Pluto-999@users.noreply.github.com>
2024-03-25 15:25:36 +0000

    Update ThirdPaneTest.java

--- ThirdPaneTest.java
+++ ThirdPaneTest.java
@@ -80,12 +80,20 @@ public class ThirdPaneTest {
         assertNotNull(label);
         assertTrue(label.getText().contains("data base does not fully cover selected period"));
     }
+
+    /**
+    * Testing the right button on the stats pane correctly functions by using the currentStatNum value and calling 
+    * the goRight ActionEvent handler method linked to the right button so as to increment the currentStatNum value.
+    */
     
     @Test
     public void testRightButtonWorks() {
-        assertEquals(1, thirdPane.currentStatNum);
-        
-        thirdPane.goRightStats(null);
+        assertEquals(1, thirdPane.currentStatNum); 
+
+        // JUnit representative function passed null as ActionEvent
+        thirdPane.goRightStats(null); 
+
+        // checking the next 2 stats are displayed
         
         assertEquals(2, thirdPane.currentStatNum);
         
@@ -94,33 +102,52 @@ public class ThirdPaneTest {
         assertEquals(3, thirdPane.currentStatNum);
         
     }
+
+    /**
+    * Testing the left button on the stats pane correctly "wraps around" such that when the user is on the first 
+    * stat and clicks the left button, the user is presented with the last stat.
+    * Using the currentStatNum value and calling the goLeft ActionEvent handler method so as to decrement the 
+    * currentStatNum value.
+    */
     
     @Test
     public void testLeftButtonWrapsAroundWorks() {
         assertEquals(1, thirdPane.currentStatNum);
-        
+
         thirdPane.goLeftStats(null);
-        
+
+        // checking the 4th stat is displayed
         assertEquals(4, thirdPane.currentStatNum);
         
     }
-    
+
+    /**
+    * Testing the right button on the stats pane correctly "wraps around" such that when the user is on the last
+    * stat and clicks the right button, the user is presented with the first stat.
+    */
     @Test
     public void testRightButtonWrapsAroundWorks() {
+        // getting the currentStatNum to equal 4
         thirdPane.goRightStats(null);
         thirdPane.goRightStats(null);
         thirdPane.goRightStats(null);
-        
+
+        // checking the 4th stat is displayed
         assertEquals(4, thirdPane.currentStatNum);
         
         thirdPane.goRightStats(null);
-        
+
+        // checking the 1st stat is displayed
         assertEquals(1, thirdPane.currentStatNum);
         
     }
-    
+
+    /**
+    * Testing the left button on the stats pane correctly functions.
+    */
     @Test
     public void testLeftButtonWorks() {
+        // setting the currentStatNum to 4
         thirdPane.goLeftStats(null);
         
         thirdPane.goLeftStats(null);
@@ -139,6 +166,4 @@ public class ThirdPaneTest {
         thirdPane = null;
     }
     
-    
-    // Similarly, write tests for generateAverageCasesLabel, generateGMR1Label, generateGMR2Label, goLeftStats, and goRightStats methods
 }
